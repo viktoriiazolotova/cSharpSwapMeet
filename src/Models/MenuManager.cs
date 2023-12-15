444c92a2bf091c71e2997ccc2c3f03ff9184888a
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2023-12-15 11:38:00 -0800

    Fix the menu options

--- src/Models/MenuManager.cs
+++ src/Models/MenuManager.cs
@@ -3,6 +3,7 @@ namespace cSharpSwapMeet
 {
     public class MenuManager
     {
+        //method to test before having FileManager
         private static List<Vendor> GetSampleVendors()
         {
             // Create and return a list of vendors with items in their inventory
@@ -16,28 +17,30 @@ namespace cSharpSwapMeet
         }
         public static void DisplayMenu()
         {
-            List<Vendor> vendors = GetSampleVendors(); // Assume vendors is populated with data
-
+            //this will read from file
+            List<Vendor> vendors = GetSampleVendors();
+            //rewrite the loop to having the do-while structure similar to pet app
             while (true)
             {
-                Console.Clear(); // Clear the console for a clean menu appearance
+                Console.Clear();
 
                 Console.WriteLine("Welcome to The Swapp Meet application");
                 Console.WriteLine("\nMenu:\n");
                 Console.WriteLine("1. List all vendors");
-                Console.WriteLine("2. Add new vendor");
-                Console.WriteLine("3. Get inventory listing for specific vendor");
-                Console.WriteLine("4. Add item to vendor's inventory");
-                Console.WriteLine("5. Remove item from vendor's inventory");
-                Console.WriteLine("6. Swap items between vendors");
-                Console.WriteLine("7. Get best item by category for a vendor");
-                Console.WriteLine("8. Swap best items between vendors");
-                Console.WriteLine("9. Add new vendor");
-                Console.WriteLine("10. Change vendor name");
-                Console.WriteLine("11. Check item availability");
-                Console.WriteLine("12. Exit");
+                Console.WriteLine("2. Add a new vendor");
+                Console.WriteLine("3. Edit a vendor name");
+                Console.WriteLine("4. Get inventory listing for the specific vendor");
+                Console.WriteLine("5. Add an item to the vendor's inventory");
+                Console.WriteLine("6. Remove an item from the vendor's inventory");
+                Console.WriteLine("7. Get all items by category for the specific vendor");
+                Console.WriteLine("8. Check item availability");
+                Console.WriteLine("9. Get best item by category for a vendor");
+                Console.WriteLine("10. Swap specific items between vendors");
+                Console.WriteLine("11. Swap best items between vendors");
+                Console.WriteLine("12. Optional - Swap first items between vendors");
+                Console.WriteLine("13. Exit");
 
-                Console.Write("\nEnter your choice (1-12): \n");
+                Console.Write("\nEnter your choice (1-13): \n");
                 string? choice = Console.ReadLine();
 
                 switch (choice)
@@ -58,10 +61,34 @@ namespace cSharpSwapMeet
                     case "4":
                         Console.WriteLine("This feature in the progress...");
                         break;
-                    // .....
-
+                    case "5":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "6":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "7":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "8":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "9":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "10":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "11":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "12":
+                        Console.WriteLine("This feature in the progress...");
+                        break;
+                    case "13":
+                        break;
                     default:
-                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.");
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 13.");
                         break;
                 }
 
--- src/obj/Debug/net8.0/cSharpSwapMeet.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/cSharpSwapMeet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpSwapMeet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4396b01fe94827ff3bc005ab623021db13ba4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b298e3408fb860b0e434a653e2bc9c1cf47b37")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpSwapMeet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpSwapMeet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
